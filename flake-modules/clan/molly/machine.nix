75666f5421bf49c1fea5a3574d3cce2a092185ea
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2025-07-23 15:28:07 +0700

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:flake-modules/clan/molly/machine.nix

--- flake-modules/clan/molly/machine.nix
+++ flake-modules/clan/molly/machine.nix
@@ -7,8 +7,15 @@
 
     services.openssh.enable = true;
 
-    users.users.root.authorizedKeys = [
+    system.stateVersion = "25.05";
+
+    boot.loader.systemd-boot = {
+      enable = true;
+    };
+
+    users.users.root.openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIImw0Xc1buEQ9WOskyGGeg3QwdbU7DTUQBiu02fObDlm" # https://github.com/jfly.keys
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBpxhiDHTsIh04+S9Pzt9p++vIvWXHDd354iDkxDOsbG" # mightyiam
     ];
 
     imports = [
