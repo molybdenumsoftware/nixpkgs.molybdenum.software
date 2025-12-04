243d378d5fff6bc9165fb4614bf23c06fab9a28a
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2025-12-04 21:23:07 +0700

    add nixos-toplevel flake check

--- flake-modules/clan/machines/molly.nix
+++ flake-modules/clan/machines/molly.nix
@@ -1,4 +1,6 @@
 { config, ... }:
 {
   clan.machines.molly = config.flake.modules.nixos.molly;
+  flake.checks.${config.flake.nixosConfigurations.molly.config.nixpkgs.hostPlatform.system}."nixos-toplevel" =
+    config.flake.nixosConfigurations.molly.config.system.build.toplevel;
 }
